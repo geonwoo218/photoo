b90794196b7cda2c73bcf53fdbba581f59ab3a3c
geonwoo218 <ekw218@naver.com>
2024-09-01 22:15:38 +0900

    selectImg
    결과화면 수정

--- src/components/selectImg.js
+++ src/components/selectImg.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const SelectedImages = ({ selectedImages }) => {
-  console.log(selectedImages.imglist);
+  
+  const [selectedTemplate, setTemplate] = useState('default');
+
+  const handleTemplateClick = (template)=>{
+    setTemplate(template);
+  }
+
   return (
     <div className='res'>
-      <div className='res-container'>
-        {selectedImages.map((img, index) => (
+      <div className={`res-container ${selectedTemplate}`}>
+        {selectedImages.map((img) => (
           <div
             key={img.id}
             className='res-imgitem'
@@ -14,7 +20,25 @@ const SelectedImages = ({ selectedImages }) => {
             <img src={img.src} alt="ss" />
           </div>
         ))}
+        <div className={`coment ${selectedTemplate}`}>
+          이야앗호
+        </div>
       </div>
+      <div className='template'>
+        {['첫번째', '두번째', '세번째', '네번째', '다섯번째', '여섯번째'].map((text, index) => (
+          <div
+            key={index}
+            className='t'
+            id={`t${index + 1}`}
+            onClick={() => handleTemplateClick(`t${index + 1}`)}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button className='endBtn'>
+        완성하기
+      </button>
     </div>
   );
 };
